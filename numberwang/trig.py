d94b7dd750fb22eab95b8b3cddcfe9bf08d80e19
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-04 18:40:29 +0000

    sine_law function can determine missing angle

--- numberwang/trig.py
+++ numberwang/trig.py
@@ -1,7 +1,7 @@
-from math import radians, sin
+from math import radians, degrees, sin, asin
 
 def sine_law(side1=None, angle1=None, side2=None, angle2=None):
     if side2 is None:
         return (side1 * sin(radians(angle2))) / sin(radians(angle1))
     elif angle2 is None:
-        return 0
+        return degrees(asin((side2 * sin(radians(angle1))) / side1))
