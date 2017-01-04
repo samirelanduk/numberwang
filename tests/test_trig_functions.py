4d7005595cfc914ff03572d1fc27697429190cc5
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-04 19:14:32 +0000

    Check sine_law arguments are numeric

--- tests/test_trig_functions.py
+++ tests/test_trig_functions.py
@@ -17,3 +17,18 @@ class SineLawTests(TestCase):
          58.99,
          delta=0.005
         )
+
+
+    def test_all_arguments_must_be_numeric(self):
+        with self.assertRaises(TypeError):
+            sine_law(side1="2", angle1=30, angle2=105)
+        sine_law(side1=2.5, angle1=30, angle2=105)
+        with self.assertRaises(TypeError):
+            sine_law(side1=2, angle1="30", angle2=105)
+        sine_law(side1=2, angle1=30.5, angle2=105)
+        with self.assertRaises(TypeError):
+            sine_law(side1=2, angle1=30, angle2="105")
+        sine_law(side1=2, angle1=30, angle2=105.5)
+        with self.assertRaises(TypeError):
+            sine_law(angle1=40, side1=30, side2="40")
+        sine_law(angle1=40, side1=30, side2=40.5)
