1719df7630037d1ca8d98d3126c8007ffb6eeb9b
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-04 16:05:47 +0000

    is_numeric docs

--- numerus/checks.py
+++ numerus/checks.py
@@ -1,4 +1,18 @@
+"""Contains functions for checking certain properties of numbers and numerical
+obects."""
+
 def is_numeric(number):
+    """Checks if an object is a number. That is, a ``float`` or an ``int``.
+    Where this differs from simply checking if an object is an instance of
+    ``numbers.Number`` is that boolean objects are also a number by that
+    measure (which, technically,
+    `they are <https://www.peterbe.com/plog/bool-is-int>`_). While technical
+    correctness is a wonderful thing, generally this is not what one would
+    expect a check for numbers to do.
+
+    :param number: The object to check.
+    :rtype: ``bool``"""
+
     if isinstance(number, bool):
         return False
     elif isinstance(number, int) or isinstance(number, float):
