0a2f56e67a31617db5d52684faf2c6d6efea6bdb
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-06 14:18:11 +0000

    Integrate are_numeric into cenral tendencies

--- numerus/stat.py
+++ numerus/stat.py
@@ -1,31 +1,31 @@
 """Tools for statistical analysis and measures of probability."""
 
 from collections import Counter
-from .checks import is_numeric
+from .checks import are_numeric
 
 def mean(*values):
     """Returns the arithmetic mean of a set of values.
 
-    :param *values: The numbers you wish to find the mean of."""
+    :param \*values: The numbers you wish to find the mean of.
+    :rtype: ``float``"""
 
     if not values:
         raise TypeError("Cannot take mean of no values")
-    for value in values:
-        if not is_numeric(value):
-            raise TypeError("'%s' is not numeric" % str(value))
+    if not are_numeric(*values):
+        raise TypeError("'%s' contains non-numeric data" % str(values))
     return sum(values) / len(values)
 
 
 def median(*values):
     """Returns the median of a set of values.
 
-    :param *values: The numbers you wish to find the median of."""
+    :param \*values: The numbers you wish to find the median of.
+    :rtype: ``float``"""
 
     if not values:
         raise TypeError("Cannot take mean of no values")
-    for value in values:
-        if not is_numeric(value):
-            raise TypeError("'%s' is not numeric" % str(value))
+    if not are_numeric(*values):
+        raise TypeError("'%s' contains non-numeric data" % str(values))
     values = sorted(values)
     length = len(values)
     if length % 2:
@@ -39,13 +39,13 @@ def mode(*values):
     """Returns the mode of a set of values. If there is more than one mode, the
     function will return ``None``.
 
-    :param *values: The numbers you wish to find the mode of."""
+    :param \*values: The numbers you wish to find the mode of.
+    :rtype: ``float``"""
 
     if not values:
         raise TypeError("Cannot take mode of no values")
-    for value in values:
-        if not is_numeric(value):
-            raise TypeError("'%s' is not numeric" % str(value))
+    if not are_numeric(*values):
+        raise TypeError("'%s' contains non-numeric data" % str(values))
     values = Counter(values)
     highest_frequency = max(values.values())
     values = [value for value in values if values[value] == highest_frequency]
