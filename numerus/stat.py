29d894f1b8d6462c336d2888a3a9fc015eaa48eb
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-06 13:18:14 +0000

    Add docs for mean function

--- numerus/stat.py
+++ numerus/stat.py
@@ -1,6 +1,12 @@
+"""Tools for statistical analysis and measures of probability."""
+
 from .checks import is_numeric
 
 def mean(*values):
+    """Returns the arithmetic mean of a set of values.
+
+    :param *values: The numbers you wish to find the mean of."""
+
     if not values:
         raise TypeError("Cannot take mean of no values")
     for value in values:
