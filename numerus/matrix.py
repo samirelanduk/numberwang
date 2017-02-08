fdcd258a0a92279458708b307dc76afa08ec425c
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-08 15:34:24 +0000

    Add sequence requirement to Matrx rows

--- numerus/matrix.py
+++ numerus/matrix.py
@@ -1,4 +1,9 @@
 class Matrix:
 
     def __init__(self, *rows):
-        self._rows = rows
+        clean_rows = []
+        for row in rows:
+            if not isinstance(row, list) and not isinstance(row, tuple):
+                raise TypeError("Matrix needs lists or tuples")
+            clean_rows.append(tuple(row))
+        self._rows = tuple(clean_rows)
--- tests/test_matrices.py
+++ tests/test_matrices.py
@@ -4,5 +4,15 @@ from numerus.matrix import Matrix
 class MatrixCreationTests(TestCase):
 
     def test_can_make_matrix(self):
-        matrix = Matrix([1, 2, 3], [4, 5, 6])
-        self.assertEqual(matrix._rows, ([1, 2, 3], [4, 5, 6]))
+        matrix = Matrix((1, 2, 3), (4, 5, 6))
+        self.assertEqual(matrix._rows, ((1, 2, 3), (4, 5, 6)))
+
+
+    def test_all_sequences_converted_to_tuple(self):
+        matrix = Matrix([1, 2, 3], (4, 5, 6))
+        self.assertEqual(matrix._rows, ((1, 2, 3), (4, 5, 6)))
+
+
+    def test_matrix_needs_sequences(self):
+        with self.assertRaises(TypeError):
+            Matrix([1, 2, 3], "456")
