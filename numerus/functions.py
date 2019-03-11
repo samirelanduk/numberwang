6f97628a9e66e2c3cac4e3e61041d2cec8d7c7bd
Sam Ireland <sam.ireland.uk@gmail.com>
2019-03-11 17:01:37 +0000

    Explain how functions work

--- numerus/functions.py
+++ numerus/functions.py
@@ -1,4 +1,28 @@
 class Function:
+    """A mathematical function, which takes an input, and returns an output.
+
+    When instantiated, zero or more 'operands' should be provided.
+
+    A Function class also has a Python lambda associated which takes two
+    arguments and returns a single value, called `func`.
+
+    When a Function instance is called, these two things - the instance's
+    operands and the class's `func` - are used together with the argument
+    actually supplied, to determine the output.
+
+    If the function instance has no operands, then the input is returned
+    unchanged.
+
+    If the function has one operand, then this value plus the input value are
+    passed to `func` and the output of this is returned.
+
+    If the function has more than one operand, then each of these operands is
+    taken in turn - if it is another function the result of this function on the
+    input value is used, otherwise the value itself is - and `func` is applied
+    to each operand cumulatively.
+
+    This base Function class acts as an identity function. Other subclasses are
+    available."""
 
     func = lambda a, b: 0
 
