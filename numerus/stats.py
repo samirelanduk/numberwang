ffcf963c2938c135477db41eca3d2aeede060ab0
Sam Ireland <sam.ireland.uk@gmail.com>
2019-03-12 16:31:23 +0000

    Add basic measures of central tendency

--- numerus/stats.py
+++ numerus/stats.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Variable:
 
     def __init__(self, *values):
@@ -24,3 +26,32 @@ class Variable:
     @property
     def length(self):
         return len(self._values)
+
+
+    @property
+    def mean(self):
+        return sum(self) / len(self) if len(self) else None
+
+
+    @property
+    def median(self):
+        values = sorted(self)
+        if not values: return None
+        index = len(self) // 2
+        if len(self) % 2:
+            return values[index]
+        else:
+            a, b = values[index - 1: index + 1]
+            return (a + b) / 2
+
+
+    @property
+    def mode(self):
+        if len(self) == 0: return None
+        counter = Counter(self)
+        most_common = counter.most_common(2)
+        if len(most_common) == 1: return most_common[0][0]
+        top_count = most_common[0][1]
+        next_count = most_common[1][1]
+        if top_count == next_count: return None
+        return most_common[0][0]
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -4,7 +4,19 @@ from unittest import TestCase
 class Tests(TestCase):
 
     def test(self):
+        # Variable creation
         heights = stats.Variable(181, 176, 187, 185, 180)
         self.assertEqual(heights.values, (181, 176, 187, 185, 180))
         self.assertEqual(heights[1], 176)
         self.assertEqual(heights.length, 5)
+
+        # Central tendency
+        self.assertEqual(heights.mean, 181.8)
+        self.assertEqual(stats.Variable().mean, None)
+        self.assertEqual(heights.median, 181)
+        self.assertEqual(stats.Variable(1, 2, 3, 4).median, 2.5)
+        self.assertEqual(stats.Variable().median, None)
+        self.assertEqual(stats.Variable(1, 2, 3, 1).mode, 1)
+        self.assertEqual(stats.Variable(1, 1, 1, 1).mode, 1)
+        self.assertEqual(heights.mode, None)
+        self.assertEqual(stats.Variable().mode, None)
