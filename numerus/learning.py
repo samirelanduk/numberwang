aa4cca3c3ada4f759eb054b0f46494803a3b0dfd
Sam Ireland <sam.ireland.uk@gmail.com>
2019-04-30 14:57:36 +0100

    Basic Model

--- numerus/learning.py
+++ numerus/learning.py
@@ -15,3 +15,39 @@ def plot_dataset(dataframe, title="", show=True):
     plt.legend()
     if show:
         plt.show()
+
+
+def split_data(df, test=0.2):
+    data = df.sample(frac=1)
+    test_cutoff = int(test * len(data))
+    return data.iloc[:-test_cutoff], data.iloc[test_cutoff:]
+
+
+
+class Model:
+
+    def __init__(self, training_data):
+        self.training_data = training_data
+        self.input_matrix = training_data.iloc[:,:-1].values
+        self.output_vector = training_data.iloc[:,-1:].values[:, 0]
+        hot = self.output_vector[0]
+        self.output_vector[self.output_vector == hot] = 1
+        self.output_vector[self.output_vector != 1] = -1
+        self.dimension = len(self.input_matrix[0])
+
+
+
+class Perceptron(Model):
+
+    def __init__(self, *args, **kwargs):
+        super(Perceptron, self).__init__(*args, **kwargs)
+        self.threshold = 0
+        self.weights = np.array([1 for _ in range(self.dimension)])
+
+
+    def __repr__(self):
+        return "<{}-dimensional Perceptron ({}-{})>".format(
+         self.dimension,
+         self.threshold,
+         ",".join(map(str, self.weights))
+        )
